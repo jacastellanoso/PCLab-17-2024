fe83231e64753a3ef8a8a2e0e06830dcf5c7f63c
Javier Alexander Castellanos Oliva <157499510+jacastellanoso@users.noreply.github.com>
2024-02-27 09:23:34 -0600

    Add files via upload

---
+++ Semana 7/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {     
+        // Variables
+        string snombre;
+        string sedad;
+        string scarrera;
+        string scarne;
+
+        //Solicitar al usuario datos
+        Console.WriteLine("Ingrese su nombre");
+        snombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su edad");
+        sedad = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carrera");
+        scarrera = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carné");
+        scarne = Console.ReadLine();
+
+
+        //Inicio de programa
+        Console.WriteLine("Mi segundo programa");
+        Console.WriteLine("Nombre: " +  snombre);
+        Console.WriteLine("Edad: " + sedad);
+        Console.WriteLine("Carrera: " +  scarrera);
+        Console.WriteLine("Carné: " + scarne);
+
+        Console.WriteLine("Es decir:");
+        Console.WriteLine("Soy " + snombre + ", tengo " + sedad + " años y estudio la carrera de " + scarrera + " y mi número de carné es " + snombre);
+        Console.ReadKey();
+    }
+}
